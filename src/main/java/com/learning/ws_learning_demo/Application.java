5b8eda57f2cf561b288e090c6de4f130aca98e36
anusha <anushaaryool@gmail.com>
2022-07-25 12:51:20 -0500

    Setup the application structure

--- src/main/java/com/learning/ws_learning_demo/Application.java
+++ src/main/java/com/learning/ws_learning_demo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WsLearningDemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WsLearningDemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
