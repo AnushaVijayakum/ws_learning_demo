2dabf441e2f98a3ad47405d3f8e3af770f244e6c
anusha <anushaaryool@gmail.com>
2022-07-25 12:15:02 -0500

    Initial commit

---
+++ src/main/java/com/learning/ws_learning_demo/WsLearningDemoApplication.java
@@ -0,0 +1,13 @@
+package com.learning.ws_learning_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsLearningDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsLearningDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/ws_learning_demo/WsLearningDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.ws_learning_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsLearningDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
